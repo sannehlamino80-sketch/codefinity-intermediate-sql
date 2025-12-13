7721889f75ea8c94890625f5396d279191218f00
sannehlamino80-sketch <>
2025-12-13 12:53:36 +0000

    Searching for the Top Math Students Challenge

---
+++ grouping/searching_for_the_top_math_students_challenge/query.sql
@@ -0,0 +1,9 @@
+SELECT 
+  student_surname, 
+  AVG(grade) AS average_grade
+FROM student_grades
+WHERE subject_name = 'Mathematics'
+  AND grade >= 90
+GROUP BY student_surname
+ORDER BY average_grade DESC
+LIMIT 10;
