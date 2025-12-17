a57048419afc1ab8e64ffd676cc5958006bfd190
sannehlamino80-sketch <>
2025-12-17 20:06:08 +0000

    Table Joins

--- joining_tables/table_joins/query.sql
+++ joining_tables/table_joins/query.sql
@@ -1,5 +1,9 @@
-SELECT category.name, SUM(product.amount) AS total_amount
-FROM product
-JOIN category ON product.category_id = category.id
-GROUP BY category.name
-ORDER BY total_amount
+SELECT
+  courses.course_id,
+  courses.course_name,
+  courses.description,
+  enrollments.student_name,
+  enrollments.enrollment_date
+FROM courses
+LEFT JOIN enrollments
+  ON courses.course_id = enrollments.course_id;
